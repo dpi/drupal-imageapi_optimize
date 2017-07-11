2a947d1678ea62f94da535b520ce88acf81f1091
Steven Jones <steven.jones@computerminds.co.uk>
2017-07-11 06:47:57 +0100

    Revert "Add the start of some tests."
    This reverts commit b0c9fd8d19719807bac42338e1c5c89d0c210dee.

--- src/ImageAPIOptimizeShellOperationsInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\iamgeapi_optimize;
-
-interface ImageAPIOptimizeShellOperationsInterface {
-  public function findExecutablePath($executable = NULL);
-  public function execShellCommand($command, $options, $arguments);
-  public function saveCommandStdoutToFile($cmd, $dst);
-}

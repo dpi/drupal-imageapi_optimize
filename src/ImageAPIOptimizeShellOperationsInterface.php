0c1eb0902a1b523cf7a8ee52590ccd015063f4f4
darthsteven <darthsteven@99644.no-reply.drupal.org>
2018-08-10 13:54:43 +0100

    Issue #2990670 by Steven Jones: Split optimizations into separate modules

--- src/ImageAPIOptimizeShellOperationsInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\imageapi_optimize;
-
-interface ImageAPIOptimizeShellOperationsInterface {
-  public function findExecutablePath($executable = NULL);
-  public function execShellCommand($command, $options, $arguments);
-  public function saveCommandStdoutToFile($cmd, $dst);
-}
